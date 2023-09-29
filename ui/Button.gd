4cabac78b78a51d139906af046b0297b847a5ba5
Martal39 <mart_lepanen@hotmail.com>
2023-09-29 22:03:10 +0300

    Hook up button to Events for sounds

--- ui/Button.gd
+++ ui/Button.gd
@@ -1,16 +1,9 @@
 extends Button
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+@export var pressed_sound : AudioStream
 
 
 func _on_pressed():
+	EventsBus.play_sfx.emit(pressed_sound)
 	SceneSwitcher.goto_scene("res://scenes/game.tscn")
 	pass # Replace with function body.
