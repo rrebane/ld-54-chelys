23dbdb6635eeb7d1dc5f78966c407b08ea93622a
Martal39 <mart_lepanen@hotmail.com>
2023-09-29 21:52:53 +0300

    Add EventsBus singleton

---
+++ singletons/EventsBus.gd
@@ -0,0 +1,8 @@
+extends Node
+
+# Singleton for using events globally
+# To add a signal, define a new signal here, e.g 'signal my_signal'
+# To emit from any other script, do EventsBus.my_signal.emit()
+# To handle signal in any script, do EventsBus.my_signal.connect(my_callback) 
+
+signal play_sfx
