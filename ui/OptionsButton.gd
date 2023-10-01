1837d4fc59dece968fec056189b5ee8ed911c370
Martal39 <mart_lepanen@hotmail.com>
2023-10-01 15:02:46 +0300

    Add how to plau

--- ui/OptionsButton.gd
+++ ui/OptionsButton.gd
@@ -2,4 +2,4 @@ extends MainMenuButton
 
 func on_pressed():
 	EventsBus.play_sfx.emit(pressed_sound)
-	SceneSwitcher.goto_scene("res://scenes/ui/options.tscn")
+	SceneSwitcher.goto_scene("res://ui/how_to_play.tscn")
