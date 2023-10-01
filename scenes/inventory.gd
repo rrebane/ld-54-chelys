f737ee8146f692bf825e05e2552c0f789055573e
Reimo Rebane <rebanerebane@gmail.com>
2023-10-01 14:46:53 +0300

    Add inventory management background

--- scenes/inventory.gd
+++ scenes/inventory.gd
@@ -3,7 +3,7 @@ extends Node2D
 @onready var item_scene_path = preload("res://scenes/item.tscn")
 
 func _process(delta):
-	if Input.is_action_just_pressed("debug_add_item"):
+	if GlobalState.debug and Input.is_action_just_pressed("debug_add_item"):
 		var item_scene = item_scene_path.instantiate()
 		$ItemContainer.add_child(item_scene)
 
